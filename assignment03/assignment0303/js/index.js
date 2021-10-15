c5e4b2e94391b730cae62cec25de3386bfb54477
weijian000 <weijian1311@gmail.com>
2021-10-16 01:28:42 +0800

    done

--- assignment03/assignment0303/js/index.js
+++ assignment03/assignment0303/js/index.js
@@ -1,16 +1,14 @@
 let elcalculateVowel = document.getElementById("calculateVowel")
 let elVowel = document.getElementById("vowel")
 let elresultVowel = document.getElementById("resultVowel")
-elcalculateVowel.addEventListener("click", function () {    
+elcalculateVowel.addEventListener("click", function () {
     elresultVowel.innerHTML = "answer is" + Vowels(elVowel.value)
 })
 
-function Vowels(){
+function Vowels() {
     let counts = 0
-    let lengths = length(document.getElementById("vowel").value)
-    for (let i = 0; i<lengths; i++)
-        if (elVowel.value.charAt(i)=="a"|elVowel.value.charAt(i)=='e'|elVowel.value.charAt(i)=='i'|elVowel.value.charAt(i)=='o'|elVowel.value.charAt(i)=='u'|elVowel.value.charAt(i)=='A'|elVowel..value.charAt(i)=='E'|elVowel.value.charAt(i)=='I'|elVowel.value.charAt(i)=='O'|elVowel.value.charAt(i)=='U') {
-            counts++
-        }
+    let lengths = document.getElementById("vowel").value.length
+    for (let i = 0; i < lengths; i++)
+        if (elVowel.value.charAt(i) == "a" | elVowel.value.charAt(i) == 'e' | elVowel.value.charAt(i) == 'i' | elVowel.value.charAt(i) == 'o' | elVowel.value.charAt(i) == 'u' | elVowel.value.charAt(i) == 'A' |elVowel.value.charAt(i) == 'E' | elVowel.value.charAt(i) == 'I' | elVowel.value.charAt(i) == 'O'| elVowel.value.charAt(i) == 'U') { counts++ }
     return counts
 }
