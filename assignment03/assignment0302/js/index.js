d477fb227586210fcc2874f52ab098199e7dc543
weijian000 <weijian1311@gmail.com>
2021-10-16 01:33:38 +0800

    minor

--- assignment03/assignment0302/js/index.js
+++ assignment03/assignment0302/js/index.js
@@ -2,13 +2,13 @@ let elcalculatePalindrome = document.getElementById("calculatePalindrome")
 let elPalindrome = document.getElementById("Palindrome")
 let elresultPalindrome = document.getElementById("resultPalindrome")
 elcalculatePalindrome.addEventListener("click", function () {
-    elresultPalindrome.innerHTML = "This is a " + palindrome(elPalindrome.value) + " Palindrome"
+    elresultPalindrome.innerHTML = "This is " + palindrome(elPalindrome.value) + " Palindrome"
 })
 
 function palindrome(number) {
     let reversed = elPalindrome.value.split("").reverse().join("")
     if (elPalindrome.value==reversed){
-        return "A"
+        return "a"
     }
     else {
         return "NOT"
