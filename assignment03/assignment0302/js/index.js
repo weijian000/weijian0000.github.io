4649c7fb9971d6cf45b6b561b5f7e3c7013c80c3
weijian000 <weijian1311@gmail.com>
2021-10-16 00:35:14 +0800

    minor chg

--- assignment03/assignment0302/js/index.js
+++ assignment03/assignment0302/js/index.js
@@ -1,12 +1,12 @@
 let elcalculatePalindrome = document.getElementById("calculatePalindrome")
-let elPalindrome = document.getElementById("Palindrome").value
+let elPalindrome = document.getElementById("Palindrome")
 let elresultPalindrome = document.getElementById("resultPalindrome")
 elcalculatePalindrome.addEventListener("click", function () {
     elresultPalindrome.innerHTML = "This is a " + palindrome(elPalindrome) + " Palindrome"
 })
 
 function palindrome(number) {
-    let reversed = elPalindrome.split("").reverse().join("")
+    let reversed = elPalindrome.toString().split("").reverse().join("")
     if (elPalindrome==reversed){
         return "A"
     }
