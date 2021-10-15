fd6c689292872df84641607fabc0f4dc23e67995
weijian000 <weijian1311@gmail.com>
2021-10-15 23:19:50 +0800

    minr chg

---
+++ A/js/index.js
@@ -0,0 +1,7 @@
+let elcalculatePalindrome = document.getElementById("calculatePalindrome")
+let elPalindrome = document.getElementById("Palindrome")
+let elresultPalindrome = document.getElementById("resultPalindrome")
+let elsa=elPalindrome.addAtribute()
+elcalculatePalindrome.addEventListener("click", function () {
+    elresultPalindrome.innerHTML = "This is a " + (e) + " Palindrome"
+})
--- assignment03/assignment0302/js/index.js
+++ assignment03/assignment0302/js/index.js
@@ -1,19 +1,17 @@
-let elcalculatePanlindrome = document.getElementById("calculatePanlindrome")
-elcalculatePanlindrome.addEventListener("click", function () {
-    let elPanlindrome = document.getElementById("Panlindrome")
-    let elresultPandolindrome = document.getElementById("resultPandolindrome")
-    elresultPandolindrome.innerHTML = "This is" + Panlindrome(elPanlindrome)
+let elcalculatePalindrome = document.getElementById("calculatePalindrome")
+let elPalindrome = document.getElementById("Palindrome").value
+let elresultPalindrome = document.getElementById("resultPalindrome")
+elcalculatePalindrome.addEventListener("click", function () {
+    elresultPalindrome.innerHTML = "This is a " + palindrome(elPalindrome) + " Palindrome"
 })
 
-let elPanlindromes = elPanlindrome.next()
-let length = elPanlindromes.length()
-function Panlindrome() {
-    for (i= length-1;i>0;i--)
-        reverse=reverse+elPanlindromes.charAt(i);
-    if (elPanlindromes.equals(reverse)){
-    console.log(true)}
-    else(
-        alert="this is not a panlindromes"
-    )
-
+function palindrome(number) {
+    let reversed = elPalindrome.split("").reverse().join("")
+    if (elPalindrome==reversed){
+        return "A"
+    }
+    else {
+        return "B"
+    }
+    
 }
